812e86f620cf8ef54878b7b1df515f555b9a0fe8
Phil Steitz <phil.steitz@gmail.com>
2023-11-12 10:48:37 -0700

    Cleanup.

--- src/main/python/supervised/train.py
+++ src/main/python/supervised/train.py
@@ -1,51 +1,67 @@
 #!/usr/bin/env python
 
 """
-train.py: trains huggingface distilbert-base-uncaset model to classify texts in hf yahoo_answers_topics dataset
+train.py: trains huggingface distilbert-base-uncaset model to classify texts in
+hf yahoo_answers_topics dataset
 """
 
 import numpy as np
 import evaluate
-from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer, AutoTokenizer, pipeline
+from transformers import AutoModelForSequenceClassification, DataCollatorWithPadding, \
+    TrainingArguments, Trainer, AutoTokenizer, pipeline
 from datasets import load_dataset
 
 # Huggingace model name for distilbert base uncased
-BERT_MODEL= "distilbert-base-uncased"
+bert_model = "distilbert-base-uncased"
 
 # load HuggingFace dataset
-YAHOO_ANSWERS_TOPICS = load_dataset("yahoo_answers_topics")
+yahoo_answers_topics = load_dataset("yahoo_answers_topics")
 
 # use AutoTokenizer for BERT_MODEL
-TOKENIZER = AutoTokenizer.from_pretrained(BERT_MODEL)
+tokenizer = AutoTokenizer.from_pretrained(bert_model)
+
 
 def preprocess_function(rec):
-    rec["text"] = rec["question_title"] + " " + rec["question_content"] + " " + rec["best_answer"]
-    return TOKENIZER(rec["text"], truncation=True)
+    """
+    Preprocesses a record from the dataset by combining the question title,
+    question content, and best answer into a single text string and then
+    tokenizing the text string using the tokenizer.
 
-TOKENIZED_YAHOO_ANSWERS_TOPICS = YAHOO_ANSWERS_TOPICS.map(preprocess_function).rename_column("topic", "labels")
+    Arguments:
+        rec - record from the dataset
+    Returns:
+        dictionary containing the tokenized text string
+    """
+    rec["text"] = rec["question_title"] + " " + \
+        rec["question_content"] + " " + rec["best_answer"]
+    return tokenizer(rec["text"], truncation=True)
 
-from transformers import DataCollatorWithPadding
 
-data_collator = DataCollatorWithPadding(tokenizer=TOKENIZER)
+tokenized_yahoo_aswers_topics = yahoo_answers_topics.map(
+    preprocess_function).rename_column("topic", "labels")
+
+
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
 accuracy = evaluate.load("accuracy")
 
-yahoo_classes = [
-           "society or culture",
-           "science or mathematics",
-           "health",
-           "education or reference",
-           "computers or internet",
-           "sports",
-           "business or finance",
-           "entertainment or music",
-           "family or relationships",
-           "politics or government"
+YAHOO_CLASSES = [
+    "society or culture",
+    "science or mathematics",
+    "health",
+    "education or reference",
+    "computers or internet",
+    "sports",
+    "business or finance",
+    "entertainment or music",
+    "family or relationships",
+    "politics or government"
 ]
 
-id2label = {i: label for i, label in enumerate(yahoo_classes)}
+id2label = {i: label for i, label in enumerate(YAHOO_CLASSES)}
 
-label2id = {label: i for i, label in enumerate(yahoo_classes)}
+label2id = {label: i for i, label in enumerate(YAHOO_CLASSES)}
+
 
 def compute_metrics(eval_pred):
     predictions, labels = eval_pred
@@ -54,7 +70,7 @@ def compute_metrics(eval_pred):
 
 
 model = AutoModelForSequenceClassification.from_pretrained(
-    BERT_MODEL, num_labels=10, id2label=id2label, label2id=label2id
+    bert_model, num_labels=10, id2label=id2label, label2id=label2id
 )
 
 training_args = TrainingArguments(
@@ -73,9 +89,10 @@ training_args = TrainingArguments(
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=TOKENIZED_YAHOO_ANSWERS_TOPICS["train"],
-    eval_dataset=TOKENIZED_YAHOO_ANSWERS_TOPICS["test"],
-    tokenizer=TOKENIZER,
+    train_dataset=tokenized_yahoo_aswers_topics["train"].shard(
+        index=0, num_shards=2),
+    eval_dataset=tokenized_yahoo_aswers_topics["test"],
+    tokenizer=tokenizer,
     data_collator=data_collator,
     compute_metrics=compute_metrics,
 )
@@ -90,10 +107,4 @@ classifier = pipeline("text-classification", model="./hf_supervised_model")
 print(classifier(text))
 # Display should look like:
 # [{'label': 'science or mathematics', 'score': 0.8060687780380249}]
-
-
-
-
-
-
-
+# with score varying depending on the training run
