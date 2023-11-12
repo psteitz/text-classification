cf32d7dceb86f05501085795b48a0aeb77caa7aa
Phil Steitz <phil.steitz@gmail.com>
2023-11-12 10:35:47 -0700

    Use defaults, cleanup.

--- src/main/python/mnli/mnli.py
+++ src/main/python/mnli/mnli.py
@@ -1,67 +1,33 @@
 from ast import List
 from datetime import datetime
 from pathlib import Path
+import os
+import sys
 import random
 import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
 from transformers.pipelines.pt_utils import KeyDataset
 from datasets import load_dataset, Dataset, load_from_disk
 
-# yahoo questions categories
-yahoo_classes = [
-           "society or culture",
-           "science or mathematics",
-           "health",
-           "education or reference",
-           "computers or internet",
-           "sports",
-           "business or finance",
-           "entertainment or music",
-           "family or relationships",
-           "politics or government"
-]
-# Alternative hypothesis templates
-DEFAULT_HYPOTHESIS_TYPE = 2
-hypothesis_type = DEFAULT_HYPOTHESIS_TYPE
-hypothesis_templates = ["This example is {}.",
-                        "This text is about {}.",
-                        "This question and answer are about {}.",
-                        "This question and answer should be classified as {}.",
-                        "The right category for this question and answer is '{}'.",
-                        "The category of this question and answer is {}.",
-                        "This question and answer relates to {}.",
-                        "This question and answer is {}.",
-                        "This question is {}.",
-                        "This question relates to {}.",
-                        "This question is about {}.",
-                        "This question should be classified as {}.",
-                        "The rigt category for this question is {}.",
-                        "The category of this question is {}.",
-                        "This text comes under the heading {}."
-                        ]
+# Set local python source root
+path_root= Path(os.environ['HOME'] + "/text-classification/src/main/python")
+sys.path.append(str(path_root))
 
-# Directory where augmented datasets are stored
-AUGMENTED_DATASET_DIR = "./hf_yahoo_data_augmented"
-
-MNLI = "facebook/bart-large-mnli"
-HF_ZERO_SHOT = "zero-shot-classification"
-SUPERVISED = "supervised"
-
-DEFAULT_SEED = 42
+from constants import defaults
 
 # Facebook BART model fine-tuned for NLI tasks
-nli_model = AutoModelForSequenceClassification.from_pretrained('facebook/bart-large-mnli', truncation=True)
-tokenizer = AutoTokenizer.from_pretrained('facebook/bart-large-mnli')
+nli_model = AutoModelForSequenceClassification.from_pretrained(defaults.HF_MNLI, truncation=True)
+tokenizer = AutoTokenizer.from_pretrained(defaults.HF_MNLI)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 nli_model.to(device)
 
-def fix_seed(seed: int = DEFAULT_SEED, with_cuda : bool = False):
+def fix_seed(seed: int = defaults.DEFAULT_SEED, with_cuda : bool = False):
     """
     Seed for the random number generator.
 
     Setting a fixed seed will make sampling-based results repeatable.
     By default, only the python random number generator is seeded.
-    Set with_cuda=True to also seed the CUDA and torch generator.
+    Set with_cuda=True to also seed the CUDA and torc generator.
 
     Arguments:
         seed - the seed value to use
@@ -73,7 +39,7 @@ def fix_seed(seed: int = DEFAULT_SEED, with_cuda : bool = False):
         torch.cuda.manual_seed_all(seed)
         torch.backends.cudnn.deterministic = True
 
-def huggingface_zero_shot_classify(sequence, labels : List(str)) -> str:
+def hf_zero_shot_classify(sequence, labels : List(str)) -> str:
     """
         Classify the sequence according to the given labels using HuggingFace zero-shot classifier.
 
@@ -103,7 +69,7 @@ def raw_nli(premise, hypothesis) -> List(float):
 
         Arguments:
         premise - the premise text
-        hypothesis - the hypothesis tex
+        hypothesis - the hypothesis text
 
         Return:
         a list of three logits [contradiction, neutral, entailmenen]
@@ -145,17 +111,17 @@ def augment_huggingface_dataset(split : str) -> Dataset:
 def simple_zero_shot_hf():
     print("HuggingFace zero-shot classifier: ")
     sequence = "What elements make up water? Oygen and hydrogen."
-    print("HuggingFace classifier: ", huggingface_zero_shot_classify(sequence, yahoo_classes))
+    print("HuggingFace classifier: ", hf_zero_shot_classify(sequence, defaults.YAHOO_CLASSES))
 
 def simple_raw_nli():
     print("Raw NLI logits for each class:")
     sequence = "What elements make up water? Oygen and hydrogen."
-    for label in yahoo_classes:
+    for label in defaults.YAHOO_CLASSES:
         hypothesis = f'This example is {label}.'
         print(f'Premise: {sequence}')
         print(f'Hypothesis: {hypothesis}')
         print(f'Raw NLI logits: {raw_nli(sequence, hypothesis)}')
-        print(f'HuggingFace zero-shot classification: {huggingface_zero_shot_classify(sequence, yahoo_classes)}')
+        print(f'HuggingFace zero-shot classification: {hf_zero_shot_classify(sequence, defaults.YAHOO_CLASSES)}')
         print()
     print("Full raw output from nli model:")
     hypothesis = 'This example is politics or government.'
@@ -207,7 +173,7 @@ def classify_pipe_directly(split: str, num_records : int = -1):
 
     # Run the model on the dataset and fill new columns from model output
     ct = 0
-    for out in classifier(KeyDataset(dataset, "text"), yahoo_classes):
+    for out in classifier(KeyDataset(dataset, "text"), defaults.YAHOO_CLASSES):
         sequence_track_column.append(hash(out["sequence"]))
         scores_column.append(out["scores"])
         labels_column.append(out["labels"])
@@ -229,16 +195,16 @@ def classify_pipe_directly(split: str, num_records : int = -1):
 
     # Save the augmented dataset to disk
     print("Saving augmented dataset to disk...")
-    Path(AUGMENTED_DATASET_DIR).mkdir(parents=True, exist_ok=True)
-    dataset.save_to_disk(AUGMENTED_DATASET_DIR)
+    Path(defaults.AUGMENTED_DATASET_DIR).mkdir(parents=True, exist_ok=True)
+    dataset.save_to_disk(defaults.AUGMENTED_DATASET_DIR)
     print("\nclassify_pipe_directly end time: " + datetime.now().strftime("%H:%M:%S"))
 
 def yahoo_index_from_text(class_label: str) -> int:
     """
     Return the index of class_label in yahoo_classes.
     """
-    for i in range(0, len(yahoo_classes)):
-        if class_label == yahoo_classes[i]:
+    for i in range(0, len(defaults.YAHOO_CLASSES)):
+        if class_label == defaults.YAHOO_CLASSES[i]:
             return i 
     print("Error: yahoo class not found: " + class_label)
     return -1
@@ -248,8 +214,8 @@ def check_hashes():
     Load the augmented dataset from disk.
     Iterate over the dataset, verifying that the hash of the text matches the hash of the sequence.
     """
-    ds = load_from_disk(AUGMENTED_DATASET_DIR)
-    print("Read ", len(ds), " records from " + AUGMENTED_DATASET_DIR)
+    ds = load_from_disk(defaults.AUGMENTED_DATASET_DIR)
+    print("Read ", len(ds), " records from " + defaults.AUGMENTED_DATASET_DIR)
     for i in range(0, len(ds)):
         if hash(ds[i]["text"]) != ds[i]["sequence_track"]:
             print("Error: hash mismatch at index " + str(i))
